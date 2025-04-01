2c7626892018b5e3e74c762a2069ae2d884aee8b
e-fa696 <e.f696@outlook.fr>
2025-04-01 15:07:13 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -38,6 +38,10 @@ def histogramme():
 @app.route("/contact/")
 def contact_form():
     return render_template("contact.html")
+      
+@app.route('/graph-commits/')
+def graph_commits():
+    return render_template("commits.html")
 
 if __name__ == "__main__":
   app.run(debug=True)
